b181244dcf5a83c23776c0bf8cb72eb6330307be
Markus Wackermann <markus.wackermann@api.de>
2022-12-29 07:50:55 +0100

    warnings

---
+++ Tests/CellTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestFixture]
+    public class CellTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
