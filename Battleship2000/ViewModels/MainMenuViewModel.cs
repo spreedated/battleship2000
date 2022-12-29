6d2d94673e4a445a1751fcce5d651b8b5718b853
SpReeDateD <webmaster@born2skate.de>
2022-12-29 06:33:40 +0100

    Add project files.

---
+++ Battleship2000/ViewModels/MainMenuViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Battleship2000.ViewLogic;
+using Battleship2000.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Battleship2000.ViewModels
+{
+    public class MainMenuViewModel : ViewModelBase
+    {
+        public ICommand ExitCommand { get; } = new RelayCommand((c) => { MainWindow.Instance.Close(); });
+    }
+}
---
+++ Battleship2000/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Battleship2000.ViewLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Battleship2000.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        public string WindowTitle { get; } = $"{((AssemblyTitleAttribute)typeof(MainWindowViewModel).Assembly.GetCustomAttributes(typeof(AssemblyTitleAttribute), false).First()).Title} v{typeof(MainWindowViewModel).Assembly.GetName().Version}";
+        public string ProjectName { get; } = $"{((AssemblyTitleAttribute)typeof(MainWindowViewModel).Assembly.GetCustomAttributes(typeof(AssemblyTitleAttribute), false).First()).Title}";
+        public string ProjectVersion { get; } = $"v{typeof(MainWindowViewModel).Assembly.GetName().Version}";
+    }
+}
