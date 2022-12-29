12a5233b30aa1243599ba3493a2b5f01c85d05c7
Markus Wackermann <markus.wackermann@api.de>
2022-12-29 07:49:51 +0100

    added tests

--- Battleship2000/AssemblyInfo.cs
+++ Battleship2000/AssemblyInfo.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using System.Windows;
 
 [assembly: ThemeInfo(
@@ -8,3 +9,4 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
+[assembly: InternalsVisibleTo("Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f91d491523712c663eb76b97c7cda787e64ba6be5d1a7841ad2865227d8172eacf2d489d587d0c56a6d7d004e9c678abe264fcfd59852382a6ca33553b6d0898ca814a8e84acf92652d2a0150f1898ae90135810dc8122d0b245edb0e87e169ebe79cea5ae15fab3613c90593eb64ce9c51de88fcc7f8e703f22d2416ec975cb")]
