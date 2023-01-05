a83523889b711311cd2c22c7f987532c1359934e
SpReeDateD <webmaster@born2skate.de>
2023-01-05 20:16:30 +0100

    added settings to preload

--- Battleship2000/Logic/Preload.cs
+++ Battleship2000/Logic/Preload.cs
@@ -1,6 +1,9 @@
 ﻿using Battleship2000.Views.Pages;
 using Serilog;
 using System;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Battleship2000.Logic
@@ -22,11 +25,17 @@ namespace Battleship2000.Logic
                 });
                 PreloadStep?.Invoke(null, EventArgs.Empty);
 
+                LoadNetworkInterfaces();
+                PreloadStep?.Invoke(null, EventArgs.Empty);
+
+                LoadBackgrounds();
+                PreloadStep?.Invoke(null, EventArgs.Empty);
+
                 PreloadComplete?.Invoke(null, EventArgs.Empty);
             });
         }
 
-        internal static void LoadPages()
+        private static void LoadPages()
         {
             Log.Information("[Preload] Loading pages...");
             ObjectStorage.pages.Add(new ConnectToServer());
@@ -38,6 +47,50 @@ namespace Battleship2000.Logic
             ObjectStorage.pages.Add(new Settings_Network());
             ObjectStorage.pages.Add(new Settings_Appearance());
             Log.Information("[Preload] Loading pages finished");
+        }
+
+        private static void LoadNetworkInterfaces()
+        {
+            Settings_Network.Instance.Dispatcher.Invoke(() =>
+            {
+                Settings_Network.Vm.NetworkInterfaces.Add(new() { IPAddress = IPAddress.Parse("0.0.0.0"), Name = "All - best option" });
+            });
+
+            foreach (NetworkInterface netInterface in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                Console.WriteLine("Name: " + netInterface.Name);
+                Console.WriteLine("Description: " + netInterface.Description);
+                Console.WriteLine("Addresses: ");
+                IPInterfaceProperties ipProps = netInterface.GetIPProperties();
+                foreach (IPAddress addr in ipProps.UnicastAddresses.Select(x => x.Address).Where(x => x.ToString() != "127.0.0.1" && x.ToString().Count(y => y == '.') == 3))
+                {
+                    Settings_Network.Instance.Dispatcher.Invoke(() =>
+                    {
+                        Settings_Network.Vm.NetworkInterfaces.Add(new() { IPAddress = addr, Name = netInterface.Name });
+                    });
+                }
+            }
+
+            Settings_Network.Instance.Dispatcher.Invoke(() =>
+            {
+                if (Settings_Network.Vm.NetworkInterfaces.Any(x => x.IPAddress.ToString() == ObjectStorage.Config.Network.Interface))
+                {
+                    Settings_Network.Instance.CMB_Interface.SelectedItem = Settings_Network.Vm.NetworkInterfaces.First(x => x.IPAddress.ToString() == ObjectStorage.Config.Network.Interface);
+                }
+            });
+        }
+
+        private static void LoadBackgrounds()
+        {
+            Settings_Appearance.Instance.Dispatcher.Invoke(() =>
+            {
+                Settings_Appearance.Vm.Backgrounds.Add(new() { Name = "Oldschool", Filename = "battleship1-1280x736.png" });
+                Settings_Appearance.Vm.Backgrounds.Add(new() { Name = "Blue", Filename = "blue.png" });
+                if (Settings_Appearance.Vm.Backgrounds.Any(x => x.Name == ObjectStorage.Config.Appearance.Background))
+                {
+                    Settings_Appearance.Instance.CMB_Background.SelectedItem = Settings_Appearance.Vm.Backgrounds.First(x => x.Name == ObjectStorage.Config.Appearance.Background);
+                }
+            });
         }
     }
 }
--- Battleship2000/ViewModels/SettingsAppearanceViewModel.cs
+++ Battleship2000/ViewModels/SettingsAppearanceViewModel.cs
@@ -2,36 +2,12 @@
 using Battleship2000.Models;
 using Battleship2000.ViewLogic;
 using Battleship2000.Views;
-using Battleship2000.Views.Pages;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Battleship2000.ViewModels
 {
     public class SettingsAppearanceViewModel : ViewModelBase
     {
-        public SettingsAppearanceViewModel()
-        {
-            Task.Factory.StartNew(async () =>
-            {
-                while (Settings_Appearance.Vm == null || Settings_Appearance.Vm.Backgrounds == null)
-                {
-                    await Task.Delay(50);
-                }
-
-                Settings_Appearance.Instance.Dispatcher.Invoke(() =>
-                {
-                    Settings_Appearance.Vm.Backgrounds.Add(new() { Name = "Oldschool", Filename = "battleship1-1280x736.png" });
-                    Settings_Appearance.Vm.Backgrounds.Add(new() { Name = "Blue", Filename = "blue.png" });
-                    if (Settings_Appearance.Vm.Backgrounds.Any(x => x.Name == ObjectStorage.Config.Appearance.Background))
-                    {
-                        Settings_Appearance.Instance.CMB_Background.SelectedItem = Settings_Appearance.Vm.Backgrounds.First(x => x.Name == ObjectStorage.Config.Appearance.Background);
-                    }
-                });
-            });
-        }
-
         public ObservableCollection<Background> Backgrounds { get; set; } = new();
 
         private Background _SelectedBackground;
