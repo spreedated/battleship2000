5e82a7eed4c045e06a8144afae275fe827cd83c7
SpReeDateD <webmaster@born2skate.de>
2023-01-21 13:24:49 +0100

    extracted method LoggerConfigurator

--- Battleship2000/App.xaml.cs
+++ Battleship2000/App.xaml.cs
@@ -3,14 +3,9 @@
 
 using Battleship2000.Logic;
 using Serilog;
-using Serilog.Events;
 using System;
-using System.IO;
-using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
-using System.Diagnostics;
-using Battleship2000.Logger.Enricher;
 
 namespace Battleship2000
 {
@@ -19,41 +14,11 @@ namespace Battleship2000
     /// </summary>
     public partial class App : Application
     {
-#if DEBUG
-        private readonly LogEventLevel level = LogEventLevel.Verbose;
-#else
-        private readonly LogEventLevel level = LogEventLevel.Information;
-#endif
         private readonly DateTime ApplicationStartupDate = DateTime.Now;
-        private const string logOutputTemplate = "[{Timestamp:HH:mm:ss} {Level:u3}][{Caller}] {Message}{NewLine}{Exception}";
-
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-
-#pragma warning disable IL3000
-            string assemblyLocation = Assembly.GetExecutingAssembly().Location;
-#pragma warning restore IL3000
-
-            if (assemblyLocation == null)
-            {
-                assemblyLocation = Environment.ProcessPath;
-            }
-
-            string logfilepath = Path.Combine(Path.GetDirectoryName(assemblyLocation), "logs", "logfile.log");
-
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Verbose()
-                .Enrich.FromLogContext()
-                .Enrich.WithCaller()
-#if DEBUG
-                .WriteTo.Debug(restrictedToMinimumLevel: level, outputTemplate: logOutputTemplate)
-#endif
-                .WriteTo.File(logfilepath, restrictedToMinimumLevel: level, rollOnFileSizeLimit: true, fileSizeLimitBytes: 1048576, outputTemplate: logOutputTemplate)
-                .CreateLogger();
-
-            Log.Debug("[App] Log initialize");
-
+            LoggerConfigurator.ConfigureLogger();
             ObjectStorage.ConfigurationHandler.Load();
         }
 
---
+++ Battleship2000/Logic/LoggerConfigurator.cs
@@ -0,0 +1,45 @@
+﻿using Battleship2000.Logger.Enricher;
+using Serilog;
+using Serilog.Events;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Battleship2000.Logic
+{
+    internal static class LoggerConfigurator
+    {
+#if DEBUG
+        private static readonly LogEventLevel level = LogEventLevel.Verbose;
+#else
+        private static readonly LogEventLevel level = LogEventLevel.Information;
+#endif
+        private const string logOutputTemplate = "[{Timestamp:HH:mm:ss} {Level:u3}][{Caller}] {Message}{NewLine}{Exception}";
+
+        internal static void ConfigureLogger()
+        {
+#pragma warning disable IL3000
+            string assemblyLocation = Assembly.GetExecutingAssembly().Location;
+#pragma warning restore IL3000
+
+            if (assemblyLocation == null)
+            {
+                assemblyLocation = Environment.ProcessPath;
+            }
+
+            string logfilepath = Path.Combine(Path.GetDirectoryName(assemblyLocation), "logs", "logfile.log");
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .Enrich.FromLogContext()
+                .Enrich.WithCaller()
+#if DEBUG
+                .WriteTo.Debug(restrictedToMinimumLevel: level, outputTemplate: logOutputTemplate)
+#endif
+                .WriteTo.File(logfilepath, restrictedToMinimumLevel: level, rollOnFileSizeLimit: true, fileSizeLimitBytes: 1048576, outputTemplate: logOutputTemplate)
+                .CreateLogger();
+
+            Log.Debug("[App] Log initialize");
+        }
+    }
+}
