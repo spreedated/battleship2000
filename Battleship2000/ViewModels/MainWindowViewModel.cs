b1e35b17f5ec4fe9400c14e3b6bde026ab68a1cc
spreedated <21370839+spreedated@users.noreply.github.com>
2023-11-12 08:45:07 +0100

    refactor mainwindowviewmodel

--- Battleship2000/ViewModels/MainWindowViewModel.cs
+++ Battleship2000/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,6 @@ using Battleship2000.Logic;
 using Battleship2000.Views;
 using Serilog;
 using System;
-using System.Linq;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,21 +24,35 @@ namespace Battleship2000.ViewModels
             RuntimeStorage.ConfigurationHandler.Save();
         });
 
-        public string WindowTitle { get; } = $"{((AssemblyTitleAttribute)typeof(MainWindowViewModel).Assembly.GetCustomAttributes(typeof(AssemblyTitleAttribute), false).First()).Title} v{typeof(MainWindowViewModel).Assembly.GetName().Version}";
-
         private ImageSource _BackgroundImage = new BitmapImage(new Uri("pack://application:,,,/Resources/blue.png"));
         public ImageSource BackgroundImage
         {
-            get
-            {
-                return _BackgroundImage;
-            }
-            set
-            {
-                _BackgroundImage = value;
-                base.OnPropertyChanged(nameof(this.BackgroundImage));
-            }
+            get => this._BackgroundImage;
+            set => base.SetProperty<ImageSource>(ref this._BackgroundImage, value);
         }
+
+        private Visibility _BackgroundVis = Visibility.Hidden;
+        public Visibility BackgroundVis
+        {
+            get => this._BackgroundVis;
+            set => base.SetProperty<Visibility>(ref this._BackgroundVis, value);
+        }
+
+        private MainWindow _Instance;
+        public MainWindow Instance
+        {
+            get => this._Instance;
+            set => base.SetProperty<MainWindow>(ref this._Instance, value);
+        }
+
+        private Page _CurrentFramePage;
+        public Page CurrentFramePage
+        {
+            get => this._CurrentFramePage;
+            set => base.SetProperty<Page>(ref this._CurrentFramePage, value);
+        }
+
+        public string WindowTitle { get; } = $"{typeof(MainWindowViewModel).Assembly.GetCustomAttribute<AssemblyTitleAttribute>().Title} v{typeof(MainWindowViewModel).Assembly.GetName().Version}";
 
         public static void Navigate(string pagename)
         {
@@ -54,48 +67,6 @@ namespace Battleship2000.ViewModels
             ((MainWindowViewModel)Application.Current.MainWindow.DataContext).CurrentFramePage = p;
 
             Log.Information($"Navigated to \"{pagename}\" page");
-        }
-
-        private Visibility _BackgroundVis = Visibility.Hidden;
-        public Visibility BackgroundVis
-        {
-            get
-            {
-                return _BackgroundVis;
-            }
-            set
-            {
-                _BackgroundVis = value;
-                base.OnPropertyChanged(nameof(this.BackgroundVis));
-            }
-        }
-
-        private MainWindow _Instance;
-        public MainWindow Instance
-        {
-            get
-            {
-                return this._Instance;
-            }
-            set
-            {
-                this._Instance = value;
-                base.OnPropertyChanged(nameof(this.Instance));
-            }
-        }
-
-        private Page _CurrentFramePage;
-        public Page CurrentFramePage
-        {
-            get
-            {
-                return this._CurrentFramePage;
-            }
-            set
-            {
-                this._CurrentFramePage = value;
-                base.OnPropertyChanged(nameof(this.CurrentFramePage));
-            }
         }
     }
 }
